bdbe292113f29f47713f559dea8e4fc16790e601
sourov pal <sourovpal35@gmail.com>
2025-12-27 12:58:24 +0600

    added fontfamily

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -1,12 +1,12 @@
 {{-- pageConfigs variable pass to Helper's updatePageConfig function to update page configuration  --}}
 @isset($pageConfigs)
-  {!! Helper::updatePageConfig($pageConfigs) !!}
+    {!! Helper::updatePageConfig($pageConfigs) !!}
 @endisset
 
 <!DOCTYPE html>
 @php
-// confiData variable layoutClasses array in Helper.php file.
-  $configData = Helper::applClasses();
+    // confiData variable layoutClasses array in Helper.php file.
+    $configData = Helper::applClasses();
 @endphp
 <!--
 Template Name: Materialize - Material Design Admin Template
@@ -21,48 +21,56 @@ License: You must have a valid license purchased only from themeforest(the above
 
 -->
 <html class="loading"
-  lang="@if(session()->has('locale')){{session()->get('locale')}}@else{{$configData['defaultLanguage']}}@endif"
-  data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}">
+    lang="@if (session()->has('locale')) {{ session()->get('locale') }}@else{{ $configData['defaultLanguage'] }} @endif"
+    data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}">
 <!-- BEGIN: Head-->
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title> @yield('title') | HPL System</title>
-  <link rel="apple-touch-icon" href="{{asset('images/favicon/apple-touch-icon-152x152.png')}}">
-  <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon/favicon-32x32.png')}}">
-
-  {{-- Include core + vendor Styles --}}
-  @include('panels.styles')
+    <title> @yield('title') | HPL System</title>
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon/apple-touch-icon-152x152.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link href="https://db.onlinewebfonts.com/c/7c6661efce01eac269383bac79303c1b?family=Arial+Narrow" rel="stylesheet">
+    <style>
+        * {
+            font-family: "Arial Narrow", sans-serif;
+        }
+    </style>
+    {{-- Include core + vendor Styles --}}
+    @include('panels.styles')
 
 </head>
 <!-- END: Head-->
 
 {{-- @isset(config('custom.custom.mainLayoutType'))
 @endisset --}}
-@if(!empty($configData['mainLayoutType']) && isset($configData['mainLayoutType']))
-  @include(($configData['mainLayoutType'] === 'horizontal-menu') ? 'layouts.horizontalLayoutMaster':'layouts.verticalLayoutMaster')
+@if (!empty($configData['mainLayoutType']) && isset($configData['mainLayoutType']))
+    @include(
+        $configData['mainLayoutType'] === 'horizontal-menu'
+            ? 'layouts.horizontalLayoutMaster'
+            : 'layouts.verticalLayoutMaster')
 @else
-{{-- if mainLaoutType is empty or not set then its print below line  --}}
-<h1>{{'mainLayoutType Option is empty in config custom.php file.'}}</h1>
+    {{-- if mainLaoutType is empty or not set then its print below line  --}}
+    <h1>{{ 'mainLayoutType Option is empty in config custom.php file.' }}</h1>
 @endif
 
 <script>
-  if ('{{ Auth::user() -> role }}' == 'internal') {
-      if ('{{ Auth::user() -> admin_role }}' != 0) {
-          $('.administration_menu').parent().find('.collapsible-sub li:first-child').hide();
-          $('.administration_menu').parent().find('.collapsible-sub li:nth-child(3)').hide();
-      }
-      $('.quotation_menu').parent().find('.collapsible-sub li:nth-child(2)').hide();
-  }
+    if ('{{ Auth::user()->role }}' == 'internal') {
+        if ('{{ Auth::user()->admin_role }}' != 0) {
+            $('.administration_menu').parent().find('.collapsible-sub li:first-child').hide();
+            $('.administration_menu').parent().find('.collapsible-sub li:nth-child(3)').hide();
+        }
+        $('.quotation_menu').parent().find('.collapsible-sub li:nth-child(2)').hide();
+    }
 
-  if ('{{ Auth::user() -> role }}' == 'external') {
-      $('.quotation_menu').parent().show()
-      $('.quotation_menu').parent().find('.collapsible-sub li:nth-child(3)').hide();
-  }
+    if ('{{ Auth::user()->role }}' == 'external') {
+        $('.quotation_menu').parent().show()
+        $('.quotation_menu').parent().find('.collapsible-sub li:nth-child(3)').hide();
+    }
 </script>
 
 </html>
